a466499361f29fe0ec705cc55389f86a56bca63b
DESKTOP-FK3FF4R\user <putri.ainur.71@gmail.com>
2022-11-15 22:51:34 +0700

    associate psychologist page finished

--- resources/views/tentang/associate-psychologist.blade.php
+++ resources/views/tentang/associate-psychologist.blade.php
@@ -15,55 +15,96 @@ nav ul li a {
         <p>Tenaga psikolog professional untuk mendampingimu berproses</p>
     </div>
     <div class="grid grid-cols-4 gap-10">
-        <div class="text-center">
-            <a href="#"
-                class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <img src="{{ asset('img/illustrations/jumbotron-home.png') }}" alt=""
-                    class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nama Psikolog 1</h5>
-                <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
-
-            </a>
+        <div
+            class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
+            <img src="{{ asset('img/psychologists/ariyanto.png') }}" alt="Ariyanto Yanwar, M.Psi., Psikolog"
+                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Ariyanto Yanwar,
+                M.Psi., Psikolog</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
         </div>
-        <div class="text-center">
-            <a href="#"
-                class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <img src="{{ asset('img/illustrations/jumbotron-home.png') }}" alt=""
-                    class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nama Psikolog 1</h5>
-                <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
-
-            </a>
+        <div
+            class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
+            <img src="{{ asset('img/psychologists/ayu.jpeg') }}" alt="Ayu Pradani S. Putri, M.Psi., Psikolog"
+                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Ayu Pradani S. Putri,
+                M.Psi., Psikolog</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
         </div>
-        <div class="text-center">
-            <a href="#"
-                class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <img src="{{ asset('img/illustrations/jumbotron-home.png') }}" alt=""
-                    class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nama Psikolog 1</h5>
-                <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
-
-            </a>
+        <div
+            class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
+            <img src="{{ asset('img/psychologists/daniswara.jpeg') }}" alt="Daniswara Agusta Wijaya, M.Psi., Psikolog"
+                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Daniswara Agusta
+                Wijaya, M.Psi., Psikolog</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
         </div>
-        <div class="text-center">
-            <a href="#"
-                class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <img src="{{ asset('img/illustrations/jumbotron-home.png') }}" alt=""
-                    class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nama Psikolog 1</h5>
-                <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
-
-            </a>
+        <div
+            class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
+            <img src="{{ asset('img/psychologists/laurentia.jpg') }}"
+                alt="Laurentia Wahyu Prastiti, S.Psi.,M.Psi.,Psikolog"
+                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Laurentia Wahyu
+                Prastiti, S.Psi.,M.Psi.,Psikolog</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
         </div>
-        <div class="text-center">
-            <a href="#"
-                class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <img src="{{ asset('img/illustrations/jumbotron-home.png') }}" alt=""
-                    class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nama Psikolog 1</h5>
-                <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
-
-            </a>
+        <div
+            class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
+            <img src="{{ asset('img/psychologists/lebda.jpeg') }}" alt="Lebda Katodhia., M.Psi., Psikolog"
+                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Lebda Katodhia.,
+                M.Psi., Psikolog</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
+        </div>
+        <div
+            class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
+            <img src="{{ asset('img/psychologists/nica.jpg') }}" alt="Eukaristianica Theofani, M.Psi., Psikolog"
+                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Eukaristianica
+                Theofani, M.Psi., Psikolog</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
+        </div>
+        <div
+            class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
+            <img src="{{ asset('img/psychologists/rininta.jpg') }}" alt="Rininta Meyftanoria, S.Psi, M.Psi, Psikolog"
+                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Rininta Meyftanoria,
+                S.Psi, M.Psi, Psikolog</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
+        </div>
+        <div
+            class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
+            <img src="{{ asset('img/psychologists/stenny.jpg') }}" alt="Stenny Prawitasari, M. Psi., Psikolog"
+                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Stenny Prawitasari, M.
+                Psi., Psikolog</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
+        </div>
+        <div
+            class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
+            <img src="{{ asset('img/psychologists/tiara.jpg') }}"
+                alt="Tiara Adjeng Endrastyana, S.Psi., M.Psi., Psikolog"
+                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Tiara Adjeng
+                Endrastyana, S.Psi., M.Psi., Psikolog</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
+        </div>
+        <div
+            class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
+            <img src="{{ asset('img/psychologists/utari.jpeg') }}" alt="Utari Krisnamurthi, M.Psi,. Psikolog"
+                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Utari Krisnamurthi,
+                M.Psi,. Psikolog</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
+        </div>
+        <div
+            class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
+            <img src="{{ asset('img/psychologists/vanessa.jpg') }}"
+                alt="Vanessa Adistiafany Pricillia, M. Psi., Psikolog"
+                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Vanessa Adistiafany
+                Pricillia, M. Psi., Psikolog</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
         </div>
     </div>
 </section>
