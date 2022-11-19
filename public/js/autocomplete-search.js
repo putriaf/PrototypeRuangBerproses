dd01cfa2acd7993a19f8bd6f7660ab4d64135118
DESKTOP-FK3FF4R\user <putri.ainur.71@gmail.com>
2022-11-19 14:08:50 +0700

    initial logic of autocomplete search

--- public/js/autocomplete-search.js
+++ public/js/autocomplete-search.js
@@ -5,7 +5,7 @@ const resultsHTML = document.getElementById("result");
 
 autocomplete.oninput = function () {
     let results = [];
-    const userInput = this.value;
+    const userInput = this.value.toLowerCase();
     resultsHTML.innerHTML = "";
     if (userInput.length > 0) {
       results = getResults(userInput);
@@ -16,24 +16,29 @@ autocomplete.oninput = function () {
     }
   };
 
-//   function getResults(input) {
-//     const results = [];
-//     fetch('https://ruangberproses-be.herokuapp.com/api/artikel-berproses')
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
-//     for (i = 0; i < data.length; i++) {
-//       if (input === data[i].slice(0, input.length)) {
-//         results.push(data[i].judul);
-//       }
-//     }
-//     return results;
-//   }
+async function getData() {
+  const response = await fetch('https://ruangberproses-be.herokuapp.com/api/artikel-berproses');
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  length = data.data.length;
+  console.log(length);
 
+  titles = [];
+  titles_lowercase = [];
+  for(i = 0; i < length; i++) {
+    titles.push(data.data[i].judul);
+    titles_lowercase.push(data.data[i].judul.toLowerCase());
+  }
+  console.log(titles);
+}
+getData();
   function getResults(input) {
     const results = [];
-    for (i = 0; i < data.length; i++) {
-      if (input === data[i].slice(0, input.length)) {
-        results.push(data[i]);
+    console.log(titles);
+    for (i = 0; i < titles.length; i++) {
+      if (input === titles_lowercase[i].slice(0, input.length)) {
+        results.push(titles[i]);
       }
     }
     return results;
