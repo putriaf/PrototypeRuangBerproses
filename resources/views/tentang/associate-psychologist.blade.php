edb4e5a52b52db622fe1b88521e6f162246b3fe6
DESKTOP-FK3FF4R\user <putri.ainur.71@gmail.com>
2022-11-23 02:48:13 +0700

    tentang fix

--- resources/views/tentang/associate-psychologist.blade.php
+++ resources/views/tentang/associate-psychologist.blade.php
@@ -9,16 +9,16 @@ nav ul li a {
 @endpush
 
 @section('content')
-<section class="lg:mx-10 lg:my-10 font-quicksand lg:mt-20">
-    <div class="text-center lg:mb-14">
-        <h1 class="text-3xl font-bold lg:mb-4">Associate Psychologist</h1>
+<section class="mx-5 lg:mx-10 lg:my-10 font-quicksand my-20 lg:mt-20">
+    <div class="text-center mb-10 lg:mb-14">
+        <h1 class="text-3xl font-bold mb-3 lg:mb-4">Associate Psychologist</h1>
         <p>Tenaga psikolog professional untuk mendampingimu berproses</p>
     </div>
-    <div class="grid grid-cols-4 gap-10">
+    <div class="grid xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-10">
         <div
             class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
             <img src="{{ asset('img/psychologists/ariyanto.png') }}" alt="Ariyanto Yanwar, M.Psi., Psikolog"
-                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+                class="w-20 h-20 rounded-full object-cover mx-auto mb-3 lg:mb-3">
             <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Ariyanto Yanwar,
                 M.Psi., Psikolog</h5>
             <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
@@ -26,7 +26,7 @@ nav ul li a {
         <div
             class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
             <img src="{{ asset('img/psychologists/ayu.jpeg') }}" alt="Ayu Pradani S. Putri, M.Psi., Psikolog"
-                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+                class="w-20 h-20 rounded-full object-cover mx-auto mb-3 lg:mb-3">
             <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Ayu Pradani S. Putri,
                 M.Psi., Psikolog</h5>
             <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
@@ -34,7 +34,7 @@ nav ul li a {
         <div
             class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
             <img src="{{ asset('img/psychologists/daniswara.jpeg') }}" alt="Daniswara Agusta Wijaya, M.Psi., Psikolog"
-                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+                class="w-20 h-20 rounded-full object-cover mx-auto mb-3 lg:mb-3">
             <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Daniswara Agusta
                 Wijaya, M.Psi., Psikolog</h5>
             <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
@@ -42,16 +42,16 @@ nav ul li a {
         <div
             class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
             <img src="{{ asset('img/psychologists/laurentia.jpg') }}"
-                alt="Laurentia Wahyu Prastiti, S.Psi.,M.Psi.,Psikolog"
-                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+                alt="Laurentia Wahyu Prastiti, S.Psi., M.Psi., Psikolog"
+                class="w-20 h-20 rounded-full object-cover mx-auto mb-3 lg:mb-3">
             <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Laurentia Wahyu
-                Prastiti, S.Psi.,M.Psi.,Psikolog</h5>
+                Prastiti, S.Psi., M.Psi., Psikolog</h5>
             <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
         </div>
         <div
             class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
             <img src="{{ asset('img/psychologists/lebda.jpeg') }}" alt="Lebda Katodhia., M.Psi., Psikolog"
-                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+                class="w-20 h-20 rounded-full object-cover mx-auto mb-3 lg:mb-3">
             <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Lebda Katodhia.,
                 M.Psi., Psikolog</h5>
             <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
@@ -59,7 +59,7 @@ nav ul li a {
         <div
             class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
             <img src="{{ asset('img/psychologists/nica.jpg') }}" alt="Eukaristianica Theofani, M.Psi., Psikolog"
-                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+                class="w-20 h-20 rounded-full object-cover mx-auto mb-3 lg:mb-3">
             <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Eukaristianica
                 Theofani, M.Psi., Psikolog</h5>
             <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
@@ -67,7 +67,7 @@ nav ul li a {
         <div
             class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
             <img src="{{ asset('img/psychologists/rininta.jpg') }}" alt="Rininta Meyftanoria, S.Psi, M.Psi, Psikolog"
-                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+                class="w-20 h-20 rounded-full object-cover mx-auto mb-3 lg:mb-3">
             <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Rininta Meyftanoria,
                 S.Psi, M.Psi, Psikolog</h5>
             <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
@@ -75,7 +75,7 @@ nav ul li a {
         <div
             class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
             <img src="{{ asset('img/psychologists/stenny.jpg') }}" alt="Stenny Prawitasari, M. Psi., Psikolog"
-                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+                class="w-20 h-20 rounded-full object-cover mx-auto mb-3 lg:mb-3">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Stenny Prawitasari, M.
                 Psi., Psikolog</h5>
             <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
@@ -84,7 +84,7 @@ nav ul li a {
             class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
             <img src="{{ asset('img/psychologists/tiara.jpg') }}"
                 alt="Tiara Adjeng Endrastyana, S.Psi., M.Psi., Psikolog"
-                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+                class="w-20 h-20 rounded-full object-cover mx-auto mb-3 lg:mb-3">
             <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Tiara Adjeng
                 Endrastyana, S.Psi., M.Psi., Psikolog</h5>
             <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
@@ -92,7 +92,7 @@ nav ul li a {
         <div
             class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
             <img src="{{ asset('img/psychologists/utari.jpeg') }}" alt="Utari Krisnamurthi, M.Psi,. Psikolog"
-                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+                class="w-20 h-20 rounded-full object-cover mx-auto mb-3 lg:mb-3">
             <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Utari Krisnamurthi,
                 M.Psi,. Psikolog</h5>
             <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
@@ -101,7 +101,7 @@ nav ul li a {
             class="text-center h-full shadow-md block p-6 max-w-sm bg-white rounded-lg border border-gray-200  hover:bg-gray-100">
             <img src="{{ asset('img/psychologists/vanessa.jpg') }}"
                 alt="Vanessa Adistiafany Pricillia, M. Psi., Psikolog"
-                class="w-20 h-20 rounded-full object-cover mx-auto lg:mb-3">
+                class="w-20 h-20 rounded-full object-cover mx-auto mb-3 lg:mb-3">
             <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Vanessa Adistiafany
                 Pricillia, M. Psi., Psikolog</h5>
             <p class="font-normal text-gray-700 dark:text-gray-400">Love relationship expert</p>
--- resources/views/tentang/profil-rb.blade.php
+++ resources/views/tentang/profil-rb.blade.php
@@ -10,32 +10,35 @@ nav ul li a {
 @endpush
 
 @section('content')
-<section class="lg:mx-10 lg:my-10 font-quicksand overflow-x-hidden lg:pt-20">
+<section
+    class="xs:mx-5 sm:mx-10 md:mx-10 lg:mx-20 lg:my-10 font-quicksand overflow-x-hidden xs:pt-20 sm:pt-20 md:pt-20 lg:pt-20">
     <div class=" text-center">
-        <h1 class="text-3xl font-bold lg:mb-4">Ruang Berproses</h1>
+        <h1 class="text-3xl font-bold xs:mb-4 sm:mb-4 md:mb-4 lg:mb-4">Ruang Berproses</h1>
         <p>Selalu ada ruang untuk kamu berproses menuju versi terbaikmu</p>
     </div>
-    <div class="flex lg:mt-10 items-end">
+    <div class="flex xs:mt-8 sm:mt-10 md:mt-10 lg:mt-10 xs:items-center md:items-end">
         <div class="flex-auto w-2/3">
-            <div class="lg:mb-8">
-                <h2 class="text-xl font-semibold lg:mb-5">Visi</h2>
-                <p>Setiap orang menyadari bahwa Kesehatan mental adalah penting dan setiap orang memiliki ruang untuk
+            <div class="xs:mb-6 sm:mb-7 md:mb-7 lg:mb-8">
+                <h2 class="text-xl font-semibold xs:mb-3 sm:mb-5 md:mb-5 lg:mb-5">Visi</h2>
+                <p class="text-justify">Setiap orang menyadari bahwa Kesehatan mental adalah penting dan setiap orang
+                    memiliki ruang untuk
                     berproses dengan dirinya.</p>
             </div>
             <div class="">
-                <h2 class="text-xl font-semibold lg:mb-5">Misi</h2>
-                <p>Mengadvokasi mengenai pentingnya akan Kesehatan mental dan mendampingi setiap orang yang ingin
+                <h2 class="text-xl font-semibold xs:mb-3 sm:mb-5 md:mb-5 lg:mb-5">Misi</h2>
+                <p class="text-justify">Mengadvokasi mengenai pentingnya akan Kesehatan mental dan mendampingi setiap
+                    orang yang ingin
                     berproses
                     dengan kondisi kesehatan mentalnya selaras dengan perubahan yang terus terjadi didalam proses
                     kehidupan
                 </p>
             </div>
         </div>
-        <div class="flex-auto w-1/3 lg:pl-32">
+        <div class="flex-auto w-1/3 xs:pl-10 md:pl-10 lg:pl-32">
             <img src="{{ asset('img/ruang-berproses/rb-only-logo.png') }}" alt="" class="">
         </div>
     </div>
-    <div class="text-center lg:mt-20">
+    <div class="text-center xs:mt-16 sm:mt-15  md:mt-20 lg:mt-20">
         <h1 class="text-3xl font-bold lg:mb-4">Perjalanan Ruang Berproses</h1>
         <section class="timeline text-white">
             <ul>
@@ -48,21 +51,24 @@ nav ul li a {
                 </li>
                 <li>
                     <div class="rounded bg-primary-dark-blue">
-                        <time>Juli 2020</time> Proin quam velit, efficitur vel neque vitae, rhoncus commodo mi.
-                        Suspendisse
-                        finibus mauris et bibendum molestie. Aenean ex augue, varius et pulvinar in, pretium non nisi.
+                        <time>Juli 2020</time>Psytalk merupakan program pertama yang dimiliki oleh Ruang Berproses.
+                        Bermula sejak Founder memberanikan diri untuk membuat webinar dengan judul 'It's okay not to be
+                        okay' dan mengundang temannya seorang Psikolog yang berada di Thailand, Priska Sinaga. Dari
+                        webinar tersebut mendapatkan Banyak feedback positif dari peserta, dan request untuk sesi
+                        lanjutan.
                     </div>
                 </li>
                 <li>
                     <div class="rounded bg-primary-dark-blue">
-                        <time>2021</time> Proin iaculis, nibh eget efficitur varius, libero tellus porta dolor, at
-                        pulvinar tortor ex eget ligula. Integer eu dapibus arcu, sit amet sollicitudin eros.
+                        <time>Februari 2022</time>Ruang berproses sudah merekrut tim lebih dari 50 orang terhitung sudah
+                        di batch 6 dan tidak hanya itu RB membuat beberapa program kreatif seperti Campaign, Brand
+                        Ambassador dan sesekali berkolaborasi dengan StartUp Psikologi lainnya.
                     </div>
                 </li>
                 <li>
                     <div class="rounded bg-primary-dark-blue">
-                        <time>2022</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse
-                        varius volutpat mattis. Vestibulum id magna est.
+                        <time>November 2022</time>Ruang Berproses memasuki masa volunteer Batch 7 dan mempunyai pengikut
+                        yang mencapai 7000 di media sosial instagram.
                     </div>
                 </li>
             </ul>
