1d6b60e0396b73c7a56274a24bbef7f255c4dc64
DESKTOP-FK3FF4R\user <putri.ainur.71@gmail.com>
2022-12-15 09:34:04 +0700

    fixes after testing

--- resources/views/Layout/layout.blade.php
+++ resources/views/Layout/layout.blade.php
@@ -6,9 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ruang Berproses</title>
-    <link rel="icon"
-        href="{{ asset('https://ruangberproses-dev.herokuapp.com/img/ruang-berproses/rb-only-logo.png') }}">
-    <link href="{{ asset('https://ruangberproses-dev.herokuapp.com/css/app.css') }}" rel="stylesheet">
+    <link rel="icon" href="{{ asset('https://ruangberproses-dev.site/img/ruang-berproses/rb-only-logo.png') }}">
+    <link href="{{ asset('https://ruangberproses-dev.site/css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
