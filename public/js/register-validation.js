2d30ca84c89c35c73f74ceffafe2fb14ac658736
DESKTOP-FK3FF4R\user <putri.ainur.71@gmail.com>
2023-01-03 08:12:23 +0700

    detail val

--- public/js/register-validation.js
+++ public/js/register-validation.js
@@ -61,7 +61,8 @@ class FormValidator {
 
         if (field.id === "no_telp") {
             const regex = new RegExp(/^[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{5,8})(?: *x(\d+))?$/);
-            if (regex.test(field.value)) {
+            const regex2 = new RegExp(/^[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4,8})(?: *x(\d+))?$/);
+            if (regex2.test(field.value)) {
                 this.setStatus(field, null, "success")
             } else {
                 this.setStatus(field, "Harap masukkan nomor telepon yang valid", "error")
