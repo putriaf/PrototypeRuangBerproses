46d37feaaa31c0a5a8fce8f1241d6fe0bf70ec2c
DESKTOP-FK3FF4R\user <putri.ainur.71@gmail.com>
2023-01-21 12:39:39 +0700

    remove unnecessary comment and add trix editor

--- resources/views/artikel/create.blade.php
+++ resources/views/artikel/create.blade.php
@@ -33,8 +33,9 @@
                     <label for="isi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Isi
                         Artikel</label>
                     <input type="text" name="isi" id="isi"
-                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        required="">
+                        class="hidden shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        required="" value="">
+                    <trix-editor input="isi" name="isi" id="isi" class=""></trix-editor>
                 </div>
                 <div class="sm:col-span-3">
                     <label for="poster"
--- resources/views/artikel/edit.blade.php
+++ resources/views/artikel/edit.blade.php
@@ -34,7 +34,7 @@
                     <label for="isi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Isi
                         Artikel</label>
                     <input type="text" name="isi" id="isi"
-                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        class="hidden shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         required="" value="{{ $artikel->isi }}">
                     <trix-editor input="isi" name="isi" id="isi" class=""></trix-editor>
                 </div>
